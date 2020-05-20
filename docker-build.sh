efc1a8d0a880c752cc8344f87ba6c39a47820105
Chen Zeng <zec@tradeshift.com>
2020-05-20 10:57:03 +0800

    fix deploy

--- docker-build.sh
+++ docker-build.sh
@@ -5,5 +5,7 @@ chmod 777 -R .
 cd public
 git init
 git remote add origin 'https://github.com/tokyo2006/tokyo2006.github.io.git' 
+git add .
+git commit -m "add new blog"
 git push --set-upstream origin master
 
