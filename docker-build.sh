5ee1df320f3675bffff88448c8a697f06e436f35
Chen Zeng <zec@tradeshift.com>
2020-05-20 10:49:42 +0800

    fix deploy

--- docker-build.sh
+++ docker-build.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
 echo "[url \"https://${GITHUB_TOKEN}:x-oauth-basic@github.com/\"]"$'\n\t'"insteadOf = https://github.com/" >> /root/.gitconfig
 hugo
-sudo chmod 777 -R .
+chmod 777 -R .
 cd public
 git init
-git remote set-url --add origin https://github.com/tokyo2006/tokyo2006.github.io.git 
+git remote set-url --add origin 'https://github.com/tokyo2006/tokyo2006.github.io.git' 
 git push --set-upstream origin master
 
